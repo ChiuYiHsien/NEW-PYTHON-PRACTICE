022fcfb98191694dca7685d567a6133083830ad0
Yi-Hsien Chiu <j546678@gmail.com>
2026-02-14 13:25:24 +0000

    new commit

---
+++ day1.py
@@ -0,0 +1,24 @@
+#first_name = "Yi-Hsien"
+#last_name = "Chiu"
+#full_name = first_name + " " + last_name
+#country = "Taiwan"
+#city = "Taipei"
+#age = 23
+#year = 2026
+#is_married = False
+#is_true = True
+#is_light_on = True#
+
+
+
+first_name, last_name, country, city, age, year, is_married, is_true, is_light_on = "Yi-Hsien", "Chiu", "Taiwan", "Taipei", 23, 2026, False, True, True
+
+
+num_1 , num_2 = 5, 4
+total = num_1 + num_2
+diff = num_1 - num_2
+product = num_1 * num_2
+division = num_1 / num_2
+
+if len(first_name) > len(country):
+    print("The length of the first name is greater than the length of the country name.")
