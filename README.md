1e21cb702e2c6a4aa6164900161e0adafc97b626
Yi-Hsien Chiu <j546678@gmail.com>
2026-02-15 18:56:47 +0000

    tuple practice

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # PYTHON-PRACTICE
 
 ITS JUST A PLACE FOR ME TO PRACTICE
+
+
+tuple : 基本上就是list，只是不能改變其值，因此能做的動作少很多
---
+++ day7.py
@@ -0,0 +1,8 @@
+family = ("father", "mother", "brother", "sister")
+print(family)
+print(len(family))
+print(family[0])
+print(family[-1])
+print(family[0:3])
+print("father" in family)
+
